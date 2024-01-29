66dd4a08937cfa412d48465a709a8cd2630dce65
Alex Kaza <61006261+klimedini@users.noreply.github.com>
2024-01-29 19:02:37 +0200

    init

---
+++ src/main/java/gr/eap/myteam/plh24ge3/Plh24GE3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.eap.myteam.plh24ge3;
+
+/**
+ *
+ * @author alex
+ */
+public class Plh24GE3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+        System.out.println("test"); 
+    }
+}
