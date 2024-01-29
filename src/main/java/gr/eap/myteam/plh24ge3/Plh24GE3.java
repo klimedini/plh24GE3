479306725d38f3a3294539697b76ad62725b6b3c
Alex Kaza <61006261+klimedini@users.noreply.github.com>
2024-01-29 20:15:37 +0200

    db init

--- src/main/java/gr/eap/myteam/plh24ge3/Plh24GE3.java
+++ src/main/java/gr/eap/myteam/plh24ge3/Plh24GE3.java
@@ -4,6 +4,10 @@
 
 package gr.eap.myteam.plh24ge3;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author alex
@@ -13,5 +17,22 @@ public class Plh24GE3 {
     public static void main(String[] args) {
         System.out.println("Hello World!"); 
         System.out.println("test"); 
+        connect();
+        
+        
+    }
+    
+    private static Connection connect(){
+        try {
+            //String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+           // Object kati = Class.forName(driver).newInstance();
+             String protocol = "jdbc:derby:";
+            String connectionString = protocol+"./derbyDB;create=true";
+            Connection connection = DriverManager.getConnection(connectionString);
+            return connection;
+        } catch (Exception ex) {
+            Logger.getLogger(Plh24GE3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
